a0bcdc90ddf1d503884ad7292700ab9f8edac75d
arinone <enzohochin@gmail.com>
2026-01-20 21:32:34 +0100

    Updated files

--- commandFunctions.py
+++ commandFunctions.py
@@ -1,4 +1,5 @@
 import random as r
+from json import loads
 
 def diceroll(dices: int, faces: int) -> str:
     dices = max(dices, 1)
@@ -8,3 +9,15 @@ def diceroll(dices: int, faces: int) -> str:
         roll = r.randint(1, faces)
         resultString += f"**Die {dice+1}** : `{roll}`\n"
     return  resultString
+
+tipsList = loads(open("./jsonfiles/tips.json").read())
+def tips() -> str:
+    rtip1 = tipsList["game"][r.randint(0, len(tipsList["game"]) - 1)]
+    rtip2 = tipsList["real"][r.randint(0, len(tipsList["real"]) - 1)]
+    result = f"""## ——Gameplay Tips——
+```{rtip1}```
+## ——Real world Tips——
+```{rtip2}```"""
+
+    return result
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import commandFunctions as cmdf
 import discord
 import logging
+from datetime import datetime
 from discord import File
 from discord.ext import commands
 from discord.ext.commands.context import Context
@@ -11,6 +12,7 @@ from os import getenv
 # Setting up logger
 logging.basicConfig(filename='jonathan_bot.log', level=logging.INFO)
 log = logging.getLogger("jonathan_bot")
+log.info(f"Jonathan bot script started at [{datetime.now()}]\n\n")
 
 # Loading environment variables
 load_dotenv(".env")
@@ -29,29 +31,41 @@ bot = commands.Bot(command_prefix=bot_cmdPrefix, description=bot_desc, intents=b
 @bot.event
 async def on_ready():
     assert bot.user is not None
+    print("Bot successfully connected")
     log.info("Bot connected :")
-    log.info(f"User : {bot.user}")
-    log.info(f"ID : {bot.user.id}")
+    log.info(f"User : [{bot.user}]")
+    log.info(f"ID : [{bot.user.id}]")
 
 #### Uncategorized ####
 @bot.command()
 async def ping(ctx: Context):
     """Checks if the bot responds."""
-    await ctx.message.reply(f"Pong ! Responsive as (nearly) always, {ctx.message.author.name}", mention_author=True)
+    await ctx.message.reply(f"Pong ! Responsive as (nearly) always, {ctx.message.author}", mention_author=True)
+    log.info(f"ping triggered by [{ctx.message.author.id}] at [{datetime.now()}]")
 
 @bot.command()
 async def roll(ctx: Context, dices: int = 1, faces: int = 20):
     """Rolls dices."""
     result = cmdf.diceroll(dices, faces)
     await ctx.send(result)
+    log.info(f"roll triggered by [{ctx.message.author.id}] at [{datetime.now()}] arg1 [{dices}] arg2 [{faces}]")
 
 @bot.command()
 async def dance(ctx: Context):
+    """Make me dance !"""
     await ctx.send(file=File("./media/jonathan_dance.gif"))
+    log.info(f"dance triggered by [{ctx.message.author.id}] at [{datetime.now()}]")
+
+@bot.command()
+async def tips(ctx: Context):
+    """Gives random Aced Beyond tips alongside some real world tips."""
+    result = cmdf.tips()
+    await ctx.send(result)
 
 @bot.command()
 async def token(ctx: Context):
     """Grabs this bot's token."""
-    await ctx.message.reply("Fuck you twin :broken_heart:", mention_author=True)
+    await ctx.message.reply("Fuh nah :broken_heart:\nYou ain't getting no tokens blud :pray::pray:", mention_author=True, file=File("./media/eeveegun.jpg"))
+    log.info(f"token triggered by [{ctx.message.author.id}] at [{datetime.now()}]")
 
 bot.run(getenv("DISCORD_TOKEN"))
