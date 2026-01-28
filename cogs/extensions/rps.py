53fee4bdd878d99cf4536e3c888b869f51e52442
ari-none <enzohochin@gmail.com>
2026-01-28 08:54:15 +0100

    Edited some of the RPS text

--- cogs/extensions/rps.py
+++ cogs/extensions/rps.py
@@ -18,37 +18,37 @@ e_match = {
 e_message = {
     "rock": {
         "scissors": "The rock breaks the scissors.",
-        "gun": "The rock breaks the gun.",
-        "fennec": "The rock smashes the fennec's skull."
+        "gun": "The rock smashes the gun, breaking it.",
+        "fennec": "The rock smashes the fennec's skull :("
     },
     "scissors": {
-        "gun": "The scissors jams the gun.",
-        "fennec": "The scissors stabs the fennec.",
+        "gun": "The scissors jams the gun (by being inserted in the gun's hole).",
+        "fennec": "The scissors stabs the fennec :(",
         "paper": "The scissors cuts the paper."
     },
     "gun": {
-        "fennec": "The gun shoots the fennec.",
-        "paper": "The gun shoots the paper.",
-        "dude": "The gun shoots the dude."
+        "fennec": "The gun shoots the fennec, killing it :(",
+        "paper": "The gun shoots the paper, piercing it.",
+        "dude": "The gun shoots the dude, killing him."
     },
     "fennec": {
-        "paper": "The fennec eats the paper.",
-        "dude": "The fennec devours the dude.",
-        "water": "The fennec drinks the water."
+        "paper": "The fennec eats the paper (dw it's edible fennec-safe paper) :3",
+        "dude": "The fennec devours the dude without leaving a trace :3",
+        "water": "The fennec drinks the water :3"
     },
     "paper": {
-        "dude": "The paper applies the dude for a job.",
+        "dude": "The paper turns into a job application & applies the dude for a job (McDonald's worker).",
         "water": "The paper absorbs the water.",
         "rock": "The paper covers the rock."
     },
     "dude": {
         "water": "The dude drinks the water.",
-        "rock": "The dude breaks the rock.",
+        "rock": "The dude breaks the rock (by picking it up & smashing it on the ground).",
         "scissors": "The dude disassembles the scissors."
     },
     "water": {
-        "rock": "The water wets the rock.",
-        "scissors": "The water rusts the scissors",
+        "rock": "The water wets the rock (Pokémon logic).",
+        "scissors": "The water rusts/oxidizes the scissors (they aren't stainless).",
         "gun": "The water jams the gun."
     }
 }
@@ -56,11 +56,11 @@ e_message = {
 # Messages for ties
 e_tie = {
     "rock": "The rocks are chilling together.",
-    "scissors": "The scissors started an arts & crafts project.",
-    "gun": "The guns are staring at each other, MENACINGLY.",
-    "fennec": "The fennecs became best friends !",
+    "scissors": "The scissors are being used by Scissor-Man.",
+    "gun": "The guns are staring at each other.\n# MENACINGLY",
+    "fennec": "The fennecs became best friends ! And eventually had a lil' fennec cub.",
     "paper": "The papers folded into airplanes & flew away.",
-    "dude": "The dudes kissed each other.",
+    "dude": "The dudes kissed each other. No explanation needed.",
     "water": "The, uhh, idk ? Water unified I guess ?"
 }
 
@@ -70,7 +70,7 @@ e_tie = {
 def match(a: str, b:str) -> bool:
     return b in e_match[a]
 
-def rps (user_choice: str, bot_choice:str) -> tuple[str, str, Color]:
+def rps (user_choice: str, bot_choice: str) -> tuple[str, str, Color]:
     """Takes the user & bot's choices. Returns a title conclusion, match conclusion and color."""
     if user_choice == bot_choice:
         return "It's a tie !", e_tie[user_choice], Color.blue()
