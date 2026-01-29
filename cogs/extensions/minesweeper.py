7cef3f5df357618f543ac3d32799a236055474b1
ari-none <enzohochin@gmail.com>
2026-01-29 14:07:51 +0100

    AProperly added minesweeper to the Games cog (finally)

--- cogs/extensions/minesweeper.py
+++ cogs/extensions/minesweeper.py
@@ -6,7 +6,17 @@ adj = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
 # Emoticon index for the symbols
 image = {
-    "#": ""
+    "#": "||<:t_e:1466133364103974973>||",
+    "~": "<:t_e:1466133364103974973>",
+    "@": "||<:t_b:1466133361914286160>||",
+    "1": "||<:t_1:1466133352045351108>||",
+    "2": "||<:t_2:1466133353441923375>||",
+    "3": "||<:t_3:1466133354544893982>||",
+    "4": "||<:t_4:1466133355736334357>||",
+    "5": "||<:t_5:1466133356885573725>||",
+    "6": "||<:t_6:1466133358164578395>||",
+    "7": "||<:t_7:1466133359175663789>||",
+    "8": "||<:t_8:1466133360643543051>||",
 }
 
 
@@ -22,7 +32,7 @@ def minesgrid(x: int=1, y: int=1) -> tuple[list[list[str]], int]:
             l.append("#") # Hashtags represents empty/safe tiles
         R.append(l)
 
-    mines = max(1, math.floor(math.sqrt(x * y)))
+    mines = max(1, math.floor(math.sqrt(x * y))) * 2
     placed = 0
 
     while placed < mines:
@@ -44,7 +54,6 @@ def numcount(grid: list[list[str]], row: int, col: int) -> int:
         if 0 <= cx < rows and 0 <= cy < cols:
             if grid[cx][cy] == "@":
                 count += 1
-
     return count
 
 def numfill(grid: list[list[str]]) -> list[list[str]]:
@@ -52,18 +61,34 @@ def numfill(grid: list[list[str]]) -> list[list[str]]:
         for col in range(len(grid[0])):
             if grid[row][col] != "@":
                 minecount = numcount(grid, row, col)
-                if minecount >= 0:
+                if minecount > 0:
                     grid[row][col] = str(minecount)
+
+    x = len(grid)
+    y = len(grid[0])
+    freeslot = max(1, math.floor(math.sqrt(x * y)))
+    placed = 0
+    maxTries = 200
+
+    while placed < freeslot:
+        row = r.randint(0, x - 1)
+        col = r.randint(0, y - 1)
+        if grid[row][col] == "#":
+            grid[row][col] = "~"  # Ats represents mines
+            placed += 1
+        maxTries -= 1
+        if maxTries <= 0:
+            break
     return grid
 
 def randomgame(rows: int, cols: int) -> str:
-    r = ""
+    ret = ""
     grid, mines = minesgrid(rows, cols)
     grid = numfill(grid)
 
-    r += f":bomb: Number of mines : "
+    ret += f"**{mines} mines on the field. Play by unspoilering the cells. If you hit a bomb, you lose. If you reveal all non-bomb tiles, you win !**\n\n"
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            r += grid[i][j]
-        r += "\n"
-    return r
+            ret += image[grid[i][j]]
+        ret += "\n"
+    return ret
